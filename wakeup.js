320f4fd2305ed3f9f16a4cd30a640d5a4525e787
William Rockwood <git@wjr.us>
2012-02-02 19:06:47 -0500

    massively modified and better.

--- wakeup.js
+++ wakeup.js
@@ -1,14 +1,10 @@
-var PlaylistName="Wake Up";
+var PlaylistName = "WakeUp";
 var iTunes = WScript.CreateObject("iTunes.Application");
 iTunes.SoundVolume=20;
-var volume=iTunes.SoundVolume;
-var LibraryName=iTunes.LibraryPlaylist.Name;
-var Playlist=iTunes.Sources.ItemByName(LibraryName).Playlists.ItemByName(PlaylistName);
-iTunes.SoundVolume=0;
+var volume = iTunes.SoundVolume;
+var LibraryName = iTunes.LibraryPlaylist.Name;
+var Playlist = iTunes.Sources.ItemByName(LibraryName).Playlists.ItemByName(PlaylistName);
+iTunes.SoundVolume = 0;
 Playlist.PlayFirstTrack();
-Playlist.Shuffle=0;
-Playlist.Shuffle=1;
-iTunes.NextTrack();
-iTunes.NextTrack();
-iTunes.SoundVolume=volume
+iTunes.SoundVolume = volume;
 iTunes.Play();
