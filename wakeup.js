da01c185f4d905639c4614126327cbdf397d4d21
William Rockwood <git@wjr.us>
2012-02-02 19:08:03 -0500

    removed volume stuff since i'm not shuffling anymore.

--- wakeup.js
+++ wakeup.js
@@ -1,10 +1,7 @@
 var PlaylistName = "WakeUp";
 var iTunes = WScript.CreateObject("iTunes.Application");
 iTunes.SoundVolume=20;
-var volume = iTunes.SoundVolume;
 var LibraryName = iTunes.LibraryPlaylist.Name;
 var Playlist = iTunes.Sources.ItemByName(LibraryName).Playlists.ItemByName(PlaylistName);
-iTunes.SoundVolume = 0;
 Playlist.PlayFirstTrack();
-iTunes.SoundVolume = volume;
 iTunes.Play();
